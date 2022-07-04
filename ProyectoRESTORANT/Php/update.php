907845d34b439c253727c362f85e54552e83be0a
BMHV <94775980+Brakmel@users.noreply.github.com>
2022-07-04 04:39:48 -0300

    Add files via upload

---
+++ ProyectoRESTORANT/Php/update.php
@@ -0,0 +1,17 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$Titulo=$_POST['Titulo'];
+$descripcion=$_POST['descripcion'];
+$imagen=$_POST['imagen'];
+$precio=$_POST['precio'];
+
+$sql="UPDATE sushi SET  descripcion='$descripcion',imagen='$imagen',precio='$precio' WHERE Titulo='$Titulo'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: Administrador.php");
+    }
+?>
